5319922d037bab08604af5b25cb4183ee42d3ec9
bach.bui <bach.bui@mservice.com.vn>
2023-07-13 14:08:44 +0700

    first commit

---
+++ Stokrypto/StokryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StokryptoApp.swift
+//  Stokrypto
+//
+//  Created by bach.bui on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct StokryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
